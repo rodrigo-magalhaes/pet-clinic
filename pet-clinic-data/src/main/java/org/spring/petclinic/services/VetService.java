eecb6f4b5e11b31c49ae7bd27f377c62d2b81ade
Rodrigo <rodrigouau@yahoo.com.br>
2018-10-02 18:51:51 -0300

    implemented interface for services. closes #5

---
+++ pet-clinic-data/src/main/java/org/spring/petclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package org.spring.petclinic.services;
+
+import org.spring.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
